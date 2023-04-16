3eb0f418251707fa78a5af17097231ceafc3c2cf
dialuponline <dialuponline@proton.me>
2023-04-16 19:30:09 +0800

    remove jshint

--- build/build_amd64.sh
+++ build/build_amd64.sh
@@ -4,3 +4,8 @@ rm -f /tmp/sandbox-python/python.so
 rm -f /tmp/sandbox-nodejs/nodejs.so
 echo "Building Python lib"
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o internal/core/runner/python/python.so -buildmode=c-shared -ldflags="-s -w" cmd/lib/python/main.go &&
+echo "Building Nodejs lib" &&
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o internal/core/runner/nodejs/nodejs.so -buildmode=c-shared -ldflags="-s -w" cmd/lib/nodejs/main.go &&
+echo "Building main" &&
+GOOS=linux GOARCH=amd64 go build -o main -ldflags="-s -w" cmd/server/main.go
+echo "Building 
