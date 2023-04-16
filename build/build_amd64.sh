623408007564e47767e5810b447af7c3d4afbbea
dialuponline <dialuponline@proton.me>
2023-04-16 18:25:53 +0800

    dccp: fix freeing skb too early for IPV6_RECVPKTINFO

--- build/build_amd64.sh
+++ build/build_amd64.sh
@@ -1,2 +1,6 @@
 rm -f internal/core/runner/python/python.so
-rm -f
+rm -f internal/core/runner/nodejs/nodejs.so
+rm -f /tmp/sandbox-python/python.so
+rm -f /tmp/sandbox-nodejs/nodejs.so
+echo "Building Python lib"
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o internal/core/runner/python/python.so -buildmode=c-shared -ldflags="-s -w" cmd/lib/python/main.go &&
