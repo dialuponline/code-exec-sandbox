3099ca9bb8a55105c7b4ce5dd25f824f36cfb519
dialuponline <dialuponline@proton.me>
2023-04-19 14:58:55 +0800

    Type checks replaced with pattern matching + some cleaning

--- build/build_arm64.sh
+++ build/build_arm64.sh
@@ -3,4 +3,7 @@ rm -f internal/core/runner/nodejs/nodejs.so
 rm -f /tmp/sandbox-python/python.so
 rm -f /tmp/sandbox-nodejs/nodejs.so
 echo "Building Python lib"
-CGO_ENABLED=1 GOOS=linux GOARCH=arm
+CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -o internal/core/runner/python/python.so -buildmode=c-shared -ldflags="-s -w" cmd/lib/python/main.go &&
+echo "Building Nodejs lib" &&
+CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -o internal/core/runner/nodejs/nodejs.so -buildmode=c-shared -ldflags="-s -w" cmd/lib/nodejs/main.go &&
+ec
