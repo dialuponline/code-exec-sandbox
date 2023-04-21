09ec1f4f5407dbf96ccd834b43954dc16e130ca0
dialuponline <dialuponline@proton.me>
2023-04-21 11:29:30 +0800

    update copyright

--- build/build_arm64.sh
+++ build/build_arm64.sh
@@ -6,4 +6,7 @@ echo "Building Python lib"
 CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -o internal/core/runner/python/python.so -buildmode=c-shared -ldflags="-s -w" cmd/lib/python/main.go &&
 echo "Building Nodejs lib" &&
 CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -o internal/core/runner/nodejs/nodejs.so -buildmode=c-shared -ldflags="-s -w" cmd/lib/nodejs/main.go &&
-ec
+echo "Building main" &&
+GOOS=linux GOARCH=arm64 go build -o main -ldflags="-s -w" cmd/server/main.go
+echo "Building env"
+GOOS=linux GOARCH=arm64 go build -o env -ldflags="-s -w" cmd/dependencies/init.go
