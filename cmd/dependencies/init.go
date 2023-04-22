d1a9c20221f92f31475c4883a660f8e767c61e2b
dialuponline <dialuponline@proton.me>
2023-04-23 06:22:35 +0800

    Prep

---
+++ cmd/dependencies/init.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/langgenius/dify-sandbox/internal/core/runner/python"
+	"github.com/langgenius/dify-sandbox/internal/static"
+	"github.com/langgenius/dify-sandbox/internal/utils/log"
+)
+
+func main() {
+	static.InitConfig("conf/config.yaml")
+
+	err := python.PreparePythonDependenciesEnv()
+	if err != nil {
+		lo
