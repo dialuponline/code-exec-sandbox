6e6db8a8892a32d89d235f2373956ecc8ea28050
dialuponline <dialuponline@proton.me>
2023-04-27 14:17:11 +0800

    Back to development: 1.5

--- cmd/test/fuzz_nodejs/main.go
+++ cmd/test/fuzz_nodejs/main.go
@@ -16,3 +16,10 @@ const (
 )
 
 func run(allowed_syscalls []int) {
+	os.Chdir("/tmp/sandbox-f5faacb9-f441-43ec-a077-a09a8b7cc7f0/var/sandbox/sandbox-nodejs/nodejs-project/node_temp/node_temp")
+
+	nums := []string{}
+	for _, syscall := range allowed_syscalls {
+		nums = append(nums, strconv.Itoa(syscall))
+	}
+	os.Setenv("ALLOWED_SYSCALLS", strings.J
