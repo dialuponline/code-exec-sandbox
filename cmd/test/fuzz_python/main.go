a2016d8c8e723e06c0797795e0571094ca4adc4c
dialuponline <dialuponline@proton.me>
2023-06-01 20:01:08 +0800

    l10n: Updates to Chinese (Taiwan) (zh_TW) translation

--- cmd/test/fuzz_python/main.go
+++ cmd/test/fuzz_python/main.go
@@ -79,4 +79,25 @@ func main() {
 
 	lock := sync.Mutex{}
 	wg := sync.WaitGroup{}
-	i 
+	i := 0
+
+	// run 4 tasks concurrently
+	for j := 0; j < 4; j++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				lock.Lock()
+				if i >= len(list) {
+					lock.Unlock()
+					return
+				}
+				task := list[i]
+				i++
+				lock.Unlock()
+				run(task)
+			}
+		}()
+	}
+
+	// wait for al
