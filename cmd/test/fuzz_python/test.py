b4d3215e873f40fb3ca4bddffc40fce0289ef9b7
dialuponline <dialuponline@proton.me>
2023-06-04 07:35:34 +0800

    Pontoon: Update Slovak (sk) localization of AMO Frontend

--- cmd/test/fuzz_python/test.py
+++ cmd/test/fuzz_python/test.py
@@ -10,4 +10,19 @@ def excepthook(type, value, tb):
     sys.stderr.flush()
     sys.exit(-1)
 
-sys.
+sys.excepthook = excepthook
+
+lib = ctypes.CDLL("/var/sandbox/sandbox-python/python.so")
+lib.DifySeccomp.argtypes = [ctypes.c_uint32, ctypes.c_uint32, ctypes.c_bool]
+lib.DifySeccomp.restype = None
+
+
+import json
+import os
+
+import json
+import sys
+import traceback
+import os
+
+os.chdir("/var/san
