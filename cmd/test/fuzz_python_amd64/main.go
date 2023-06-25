4979b22f7bd8fdd7bc1f887f3e1b039ff9ced7f4
dialuponline <dialuponline@proton.me>
2023-06-25 11:26:51 +0800

    Added stylesheet

--- cmd/test/fuzz_python_amd64/main.go
+++ cmd/test/fuzz_python_amd64/main.go
@@ -8,4 +8,17 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/langgenius/dify-sandbox/inte
+	"github.com/langgenius/dify-sandbox/internal/static/python_syscall"
+)
+
+const (
+	SYSCALL_NUMS = 400
+)
+
+func run(allowed_syscalls []int) {
+	nums := []string{}
+	for _, syscall := range allowed_syscalls {
+		nums = append(nums, strconv.Itoa(syscall))
+	}
+	os.Setenv("ALLOWED_SYSCALLS", strings.Join(nums, ","))
+	_, err := exec.Command("
