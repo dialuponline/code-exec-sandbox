1e3747fcec5d0ebc4491804f70925f0d8ede1104
dialuponline <dialuponline@proton.me>
2023-10-01 16:42:59 +0800

    Create teste

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-# chec
+# check if ubuntu/debain
+if [ -f /etc/debian_version ]; then
+    sudo apt-get install pkg-config gcc libseccomp-dev
+# check if fedora
+elif [ -f /etc/fedora-release ]; then
+    sudo dnf install pkgconfig gcc libseccomp-devel
+# check if arch
+elif [ -f /etc/arch-release 
