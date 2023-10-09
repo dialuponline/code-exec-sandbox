39f3a4a82786685eef964ba95b8fd0c449307b1d
dialuponline <dialuponline@proton.me>
2023-10-10 06:29:18 +0800

    Add namespace Typescript definition

--- internal/controller/run.go
+++ internal/controller/run.go
@@ -2,4 +2,11 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	runner_ty
+	runner_types "github.com/langgenius/dify-sandbox/internal/core/runner/types"
+	"github.com/langgenius/dify-sandbox/internal/service"
+	"github.com/langgenius/dify-sandbox/internal/types"
+)
+
+func RunSandboxController(c *gin.Context) {
+	BindRequest(c, func(req struct {
+		Language      string `json:"langua
