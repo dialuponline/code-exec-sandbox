01f8011cbacee2be87452f059f49bb43c92bcc37
dialuponline <dialuponline@proton.me>
2023-10-15 06:31:05 +0800

    AppVeyor

--- internal/controller/run.go
+++ internal/controller/run.go
@@ -9,4 +9,11 @@ import (
 
 func RunSandboxController(c *gin.Context) {
 	BindRequest(c, func(req struct {
-		Language      string `json:"langua
+		Language      string `json:"language" form:"language" binding:"required"`
+		Code          string `json:"code" form:"code" binding:"required"`
+		Preload       string `json:"preload" form:"preload"`
+		EnableNetwork bool   `json:"enable_network" form:"enable_network"`
+	}) {
+		switch req.Language {
+		case "python3":
+			c.JSON(200, 
