e897c7fc73e1d579f25941a039872fe0a0744891
dialuponline <dialuponline@proton.me>
2023-10-29 15:23:39 +0800

    Fix a compilation warning.

--- internal/core/lib/python/add_seccomp.go
+++ internal/core/lib/python/add_seccomp.go
@@ -16,4 +16,17 @@ func InitSeccomp(uid int, gid int, enable_network bool) error {
 	err := syscall.Chroot(".")
 	if err != nil {
 		return err
-	
+	}
+	err = syscall.Chdir("/")
+	if err != nil {
+		return err
+	}
+
+	lib.SetNoNewPrivs()
+
+	allowed_syscalls := []int{}
+	allowed_not_kill_syscalls := []int{}
+	allowed_not_kill_syscalls = append(allowed_not_kill_syscalls, python_syscall.ALLOW_ERROR_SYSCALLS...)
+
+	allowed_syscall := os.Getenv("ALLOWED_SYSCALLS")
+	if all
