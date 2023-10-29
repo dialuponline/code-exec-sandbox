24408eda27a69ee226e9ad3b373c23bd32606aad
dialuponline <dialuponline@proton.me>
2023-10-29 14:47:53 +0800

    jfs: fix error path in ialloc

--- internal/core/lib/python/add_seccomp.go
+++ internal/core/lib/python/add_seccomp.go
@@ -1,4 +1,19 @@
 package python
 
 import (
-	"o
+	"os"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"github.com/langgenius/dify-sandbox/internal/core/lib"
+	"github.com/langgenius/dify-sandbox/internal/static/python_syscall"
+)
+
+//var allow_syscalls = []int{}
+
+func InitSeccomp(uid int, gid int, enable_network bool) error {
+	err := syscall.Chroot(".")
+	if err != nil {
+		return err
+	
