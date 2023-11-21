55f1aa0e5330ebb734a0604beee6f83212663373
dialuponline <dialuponline@proton.me>
2023-11-22 05:35:44 +0800

    Trim.

--- internal/core/runner/nodejs/nodejs.go
+++ internal/core/runner/nodejs/nodejs.go
@@ -12,4 +12,19 @@ import (
 
 	"github.com/langgenius/dify-sandbox/internal/core/runner"
 	"github.com/langgenius/dify-sandbox/internal/core/runner/types"
-	"github.com/langgenius/dify-sa
+	"github.com/langgenius/dify-sandbox/internal/static"
+)
+
+type NodeJsRunner struct {
+	runner.TempDirRunner
+}
+
+//go:embed prescript.js
+var nodejs_sandbox_fs []byte
+
+var (
+	REQUIRED_FS = []string{
+		path.Join(LIB_PATH, PROJECT_NAME, "node_temp"),
+		path.Join(LIB_PATH, LIB_NAME),
+		"/etc/ssl/certs/ca-certificates.crt",
+		"/etc/nsswitch.
