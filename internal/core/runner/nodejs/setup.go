17c2fced44ec37dba9b45aa89f48c2aa50857f8f
dialuponline <dialuponline@proton.me>
2023-12-17 17:20:12 +0800

    blkcg: don't call into policy draining if root_blkg is already gone

--- internal/core/runner/nodejs/setup.go
+++ internal/core/runner/nodejs/setup.go
@@ -27,4 +27,13 @@ func init() {
 
 func releaseLibBinary() {
 	log.Info("initializing nodejs runner environment...")
-	
+	os.RemoveAll(LIB_PATH)
+	os.Remove(LIB_PATH)
+
+	err := os.MkdirAll(LIB_PATH, 0755)
+	if err != nil {
+		log.Panic(fmt.Sprintf("failed to create %s", LIB_PATH))
+	}
+	err = os.WriteFile(path.Join(LIB_PATH, LIB_NAME), nodejs_lib, 0755)
+	if err != nil {
+		log.Panic(fmt.Sprintf("failed to write %s", path.Join(LIB_PATH, 
