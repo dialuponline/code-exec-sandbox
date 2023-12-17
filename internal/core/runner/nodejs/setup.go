ca7975738d02f65f7e4fc63a5ee682c43c9dcb29
dialuponline <dialuponline@proton.me>
2023-12-17 14:50:29 +0800

    after-merge fix

--- internal/core/runner/nodejs/setup.go
+++ internal/core/runner/nodejs/setup.go
@@ -10,4 +10,21 @@ import (
 )
 
 const (
-	LIB_PATH     = "/var/sandbox/s
+	LIB_PATH     = "/var/sandbox/sandbox-nodejs"
+	LIB_NAME     = "nodejs.so"
+	PROJECT_NAME = "nodejs-project"
+)
+
+//go:embed nodejs.so
+var nodejs_lib []byte
+
+//go:embed dependens
+var nodejs_dependens embed.FS // it's a directory
+
+func init() {
+	releaseLibBinary()
+}
+
+func releaseLibBinary() {
+	log.Info("initializing nodejs runner environment...")
+	
