8ab4a72b5573d522cf3baa5e93fbb483c9bb4c74
dialuponline <dialuponline@proton.me>
2024-02-04 11:25:06 +0800

    [osx] sync xcode project

--- internal/core/runner/python/env.go
+++ internal/core/runner/python/env.go
@@ -7,4 +7,16 @@ import (
 	"path"
 
 	"github.com/langgenius/dify-sandbox/internal/core/runner"
-	"github.com/langgenius/dify-sandbox/intern
+	"github.com/langgenius/dify-sandbox/internal/static"
+	"github.com/langgenius/dify-sandbox/internal/utils/log"
+)
+
+//go:embed env.sh
+var env_script string
+
+func PreparePythonDependenciesEnv() error {
+	config := static.GetDifySandboxGlobalConfigurations()
+
+	runner := runner.TempDirRunner{}
+	err := runner.WithTempDir("/", []string{}, func(root_path string) error {
+		err
