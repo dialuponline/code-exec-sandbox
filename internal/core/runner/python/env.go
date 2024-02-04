1a0d038ec29bba3143c1359b8c3773e62cd55e90
dialuponline <dialuponline@proton.me>
2024-02-04 14:33:42 +0800

    updated package info

--- internal/core/runner/python/env.go
+++ internal/core/runner/python/env.go
@@ -19,4 +19,12 @@ func PreparePythonDependenciesEnv() error {
 
 	runner := runner.TempDirRunner{}
 	err := runner.WithTempDir("/", []string{}, func(root_path string) error {
-		err
+		err := os.WriteFile(path.Join(root_path, "env.sh"), []byte(env_script), 0755)
+		if err != nil {
+			return err
+		}
+
+		for _, lib_path := range config.PythonLibPaths {
+			// check if the lib path is available
+			if _, err := os.Stat(lib_path); err != nil {
+				log.Warn("python lib path %s is not available", lib_path)
