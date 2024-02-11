7ed48d782478556df6b2b2dd32cad45e37516113
dialuponline <dialuponline@proton.me>
2024-02-11 09:37:28 +0800

    Minor refactor.

--- internal/core/runner/python/env.sh
+++ internal/core/runner/python/env.sh
@@ -14,4 +14,10 @@ copy_and_link() {
     local src_file="$1"
     local dest_file="$2"
 
-    if [ -L "$src_file" ]
+    if [ -L "$src_file" ]; then
+        # If src_file is a symbolic link, copy it without changing permissions
+        cp -P "$src_file" "$dest_file"
+    elif [ -b "$src_file" ] || [ -c "$src_file" ]; then
+        # If src_file is a device file, copy it and change permissions
+        cp "$src_file" "$dest_file"
+        ch
