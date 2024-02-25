a602805ceaffa575844e447cb76f211892c05f5a
dialuponline <dialuponline@proton.me>
2024-02-25 21:48:47 +0800

    fixes #23

--- internal/core/runner/python/prescript.py
+++ internal/core/runner/python/prescript.py
@@ -5,3 +5,16 @@ import traceback
 # setup sys.excepthook
 def excepthook(type, value, tb):
     sys.stderr.write("".join(traceback.format_exception(type, value, tb)))
+    sys.stderr.flush()
+    sys.exit(-1)
+
+sys.excepthook = excepthook
+
+lib = ctypes.CDLL("./var/sandbox/sandbox-python/python.so")
+lib.DifySeccomp.argtypes = [ctypes.c_uint32, ctypes.c_uint32, ctypes.c_bool]
+lib.DifySeccomp.restype = None
+
+# get running path
+running_path = sys.argv[1]
+if not running_path:
+ 
