15f1235fe46e2b4c68c0acddf86474e8c34a62f7
dialuponline <dialuponline@proton.me>
2024-03-03 19:22:30 +0800

    Update term-Finder.md

--- internal/core/runner/temp_dir.go
+++ internal/core/runner/temp_dir.go
@@ -5,4 +5,16 @@ import (
 	"os/exec"
 	"path"
 
-	"github.com/goo
+	"github.com/google/uuid"
+)
+
+type TempDirRunner struct{}
+
+func (s *TempDirRunner) WithTempDir(basedir string, paths []string, closures func(path string) error) error {
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		return err
+	}
+
+	// create a tmp dir
+	tmp_dir := path.Join(basedir, "tmp", "sandbo
