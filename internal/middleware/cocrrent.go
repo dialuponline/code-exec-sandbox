ad726af247095564e637d26ba4b772b4c71a06cb
dialuponline <dialuponline@proton.me>
2024-03-17 10:29:03 +0800

    removed junk

--- internal/middleware/cocrrent.go
+++ internal/middleware/cocrrent.go
@@ -4,4 +4,17 @@ import (
 	"net/http"
 	"sync"
 
-	"github.com/gin-goni
+	"github.com/gin-gonic/gin"
+	"github.com/langgenius/dify-sandbox/internal/types"
+	"github.com/langgenius/dify-sandbox/internal/utils/log"
+)
+
+func MaxWorker(max int) gin.HandlerFunc {
+	log.Info("setting max workers to %d", max)
+	sem := make(chan struct{}, max)
+
+	return func(c *gin.Context) {
+		sem <- struct{}{}
+		defer func() {
+			<-sem
+		}
