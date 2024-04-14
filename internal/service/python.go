7a2892429f2c26af240749e9cd3f67a16d41a735
dialuponline <dialuponline@proton.me>
2024-04-14 11:59:24 +0800

    fix list

--- internal/service/python.go
+++ internal/service/python.go
@@ -3,4 +3,12 @@ package service
 import (
 	"time"
 
-	"github.com/langgenius/dif
+	"github.com/langgenius/dify-sandbox/internal/core/runner/python"
+	runner_types "github.com/langgenius/dify-sandbox/internal/core/runner/types"
+	"github.com/langgenius/dify-sandbox/internal/static"
+	"github.com/langgenius/dify-sandbox/internal/types"
+)
+
+type RunCodeResponse struct {
+	Stderr string `json:"error"`
+	Stdout string `json:"st
