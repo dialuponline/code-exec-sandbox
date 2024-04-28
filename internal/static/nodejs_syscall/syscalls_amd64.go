d17097853e64798063a154cf4b411f87376c5818
dialuponline <dialuponline@proton.me>
2024-04-28 19:13:18 +0800

    first comment

--- internal/static/nodejs_syscall/syscalls_amd64.go
+++ internal/static/nodejs_syscall/syscalls_amd64.go
@@ -7,4 +7,13 @@ import "syscall"
 const (
 	//334
 	SYS_RSEQ = 334
-	// 43
+	// 435
+	SYS_CLONE3 = 435
+)
+
+var ALLOW_SYSCALLS = []int{
+	syscall.SYS_OPEN, syscall.SYS_WRITE, syscall.SYS_CLOSE, syscall.SYS_READ,
+	syscall.SYS_OPENAT, syscall.SYS_NEWFSTATAT, syscall.SYS_IOCTL, syscall.SYS_LSEEK,
+	syscall.SYS_FSTAT,
+	syscall.SYS_MPROTECT, syscall.SYS_MMAP, syscall.SYS_MUNMAP,
+	sysca
