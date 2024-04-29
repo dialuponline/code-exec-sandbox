8a9cc0d6f46df56a5120442ebb74f95b6a5dd7a6
dialuponline <dialuponline@proton.me>
2024-04-30 06:39:32 +0800

    ASoC: ak4642: prevent un-necessary changes to SG_SL1

--- internal/static/nodejs_syscall/syscalls_amd64.go
+++ internal/static/nodejs_syscall/syscalls_amd64.go
@@ -16,4 +16,11 @@ var ALLOW_SYSCALLS = []int{
 	syscall.SYS_OPENAT, syscall.SYS_NEWFSTATAT, syscall.SYS_IOCTL, syscall.SYS_LSEEK,
 	syscall.SYS_FSTAT,
 	syscall.SYS_MPROTECT, syscall.SYS_MMAP, syscall.SYS_MUNMAP,
-	sysca
+	syscall.SYS_MREMAP,
+	syscall.SYS_BRK,
+	syscall.SYS_RT_SIGACTION, syscall.SYS_RT_SIGPROCMASK,
+	syscall.SYS_MADVISE, syscall.SYS_GETPID, syscall.SYS_GETUID,
+	syscall.SYS_FCNTL, syscall.SYS_SIGALTSTACK, syscall.SYS_RT_SIGRETURN,
+	syscall.SYS_FUTEX,
+	syscall.SYS_EXIT_GROUP,
+	syscall.SYS_EPOL
