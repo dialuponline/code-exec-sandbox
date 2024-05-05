19ab5e0bbe41478858e2cc279c14b256d21159ad
dialuponline <dialuponline@proton.me>
2024-05-05 16:56:37 +0800

    dp

--- internal/static/nodejs_syscall/syscalls_amd64.go
+++ internal/static/nodejs_syscall/syscalls_amd64.go
@@ -23,4 +23,15 @@ var ALLOW_SYSCALLS = []int{
 	syscall.SYS_FCNTL, syscall.SYS_SIGALTSTACK, syscall.SYS_RT_SIGRETURN,
 	syscall.SYS_FUTEX,
 	syscall.SYS_EXIT_GROUP,
-	syscall.SYS_EPOL
+	syscall.SYS_EPOLL_CTL,
+	syscall.SYS_EPOLL_PWAIT,
+	syscall.SYS_SCHED_YIELD, syscall.SYS_EXIT,
+	syscall.SYS_SCHED_GETAFFINITY, syscall.SYS_SET_ROBUST_LIST,
+	SYS_RSEQ,
+
+	syscall.SYS_SETUID, syscall.SYS_SETGID, syscall.SYS_GETTID,
+
+	syscall.SYS_CLOCK_GETTIME, syscall.SYS_GETTIMEOFDAY, syscall.SYS_NANOSLEEP,
+	syscall.SYS_TIME,
+
+	syscall.SYS_TGKILL
