aeb432d56732724a94fea7e4520880fada67c5b9
dialuponline <dialuponline@proton.me>
2024-05-17 11:18:14 +0800

    more merge fixes

--- internal/types/config.go
+++ internal/types/config.go
@@ -1,3 +1,12 @@
 package types
 
-type DifySandboxGlobalConfigurations struct 
+type DifySandboxGlobalConfigurations struct {
+	App struct {
+		Port  int    `yaml:"port"`
+		Debug bool   `yaml:"debug"`
+		Key   string `yaml:"key"`
+	} `yaml:"app"`
+	MaxWorkers      int      `yaml:"max_workers"`
+	MaxRequests     int      `yaml:"max_requests"`
+	WorkerTimeout   int      `yaml:"worker_timeout"`
+	PythonPath      string   `y
