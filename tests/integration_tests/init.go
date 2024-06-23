62c24c8c8654a0f5677bf711eb0380c36d5fd371
dialuponline <dialuponline@proton.me>
2024-06-23 20:29:41 +0800

    REN: the file has been renamed to meet the PHP coding guidelines.

--- tests/integration_tests/init.go
+++ tests/integration_tests/init.go
@@ -1,4 +1,14 @@
 package integrationtests_test
 
 import (
-	"github.com/langgenius/dify-sandbox/internal/core/
+	"github.com/langgenius/dify-sandbox/internal/core/runner/python"
+	"github.com/langgenius/dify-sandbox/internal/static"
+	"github.com/langgenius/dify-sandbox/internal/utils/log"
+)
+
+func init() {
+	static.InitConfig("conf/config.yaml")
+
+	err := python.PreparePythonDependenciesEnv()
+	if err != nil {
+		log.Panic("failed to initialize python dependenc
