b8e80920961874cc2284b9c743a176b7d2b166cb
dialuponline <dialuponline@proton.me>
2024-07-01 19:59:34 +0800

    Remove debugging lines

--- tests/integration_tests/nodejs_feature_test.go
+++ tests/integration_tests/nodejs_feature_test.go
@@ -10,3 +10,15 @@ import (
 
 func TestNodejsBase64(t *testing.T) {
 	// Test case for base64
+	runMultipleTestings(t, 30, func(t *testing.T) {
+		resp := service.RunNodeJsCode(`
+const base64 = Buffer.from("hello world").toString("base64");
+console.log(Buffer.from(base64, "base64").toString());
+		`, "", &types.RunnerOptions{
+			EnableNetwork: true,
+		})
+		if resp.Code != 0 {
+			t.Fatal(resp)
+		}
+
+		if resp.Data.
