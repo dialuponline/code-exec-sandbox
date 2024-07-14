6ff14c78924050e20c964c544aa7fbb0f1d1eb1e
dialuponline <dialuponline@proton.me>
2024-07-14 16:05:52 +0800

    Update sample.

--- tests/integration_tests/python_feature_test.go
+++ tests/integration_tests/python_feature_test.go
@@ -1,3 +1,16 @@
 package integrationtests_test
 
-import 
+import (
+	"strings"
+	"testing"
+
+	"github.com/langgenius/dify-sandbox/internal/core/runner/types"
+	"github.com/langgenius/dify-sandbox/internal/service"
+)
+
+func TestPythonBase64(t *testing.T) {
+	// Test case for base64
+	runMultipleTestings(t, 50, func(t *testing.T) {
+		resp := service.RunPython3Code(`
+import base64
+print(base6
