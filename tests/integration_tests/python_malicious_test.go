1ff72d5edbaf65ac946e7bb15c08ee1d8108f747
dialuponline <dialuponline@proton.me>
2024-08-11 11:47:43 +0800

    Commit automatically

--- tests/integration_tests/python_malicious_test.go
+++ tests/integration_tests/python_malicious_test.go
@@ -1,1 +1,17 @@
-package
+package integrationtests_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/langgenius/dify-sandbox/internal/core/runner/types"
+	"github.com/langgenius/dify-sandbox/internal/service"
+)
+
+func TestSysFork(t *testing.T) {
+	// Test case for sys_fork
+	resp := service.RunPython3Code(`
+import os
+print(os.fork())
+print(123)
+	`, "", &ty
